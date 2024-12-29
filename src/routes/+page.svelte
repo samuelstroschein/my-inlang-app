2cc6d19a755d89c539807dfd63d4a7064e084913
LightsAppDev <152993807+LightsAppDev@users.noreply.github.com>
2024-12-29 20:53:13 +0200

    simple svelte inlang app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    	import * as m from '$lib/paraglide/messages.js';
+    </script>
+
+<h1>{m.welcome_title()}</h1>
+<p>{m.main_page_paragraph()}</p>
+
---
+++ src/routes/about-us/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    	import * as m from '$lib/paraglide/messages.js';
+</script>
+
+<h1>{m.about_us_title()}</h1>
+<p>{m.about_us_paragraph}</p>
