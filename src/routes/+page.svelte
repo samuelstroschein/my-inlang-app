8ba2eeaa8ca23664dfb3f6c9ffa0fc088e8190ca
LightsAppDev <152993807+LightsAppDev@users.noreply.github.com>
2024-12-30 09:31:01 +0200

    add nav links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     	import * as m from '$lib/paraglide/messages.js';
     </script>
+<div>pages:   <a href="/">Home</a> |  <a href="/about-us">About us</a></div>
 
 <h1>{m.welcome_title()}</h1>
 <p>{m.main_page_paragraph()}</p>
--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     	import * as m from '$lib/paraglide/messages.js';
 </script>
+<div>pages:   <a href="/">Home</a> |  <a href="about-us">About us</a></div>
 
 <h1>{m.about_us_title()}</h1>
-<p>{m.about_us_paragraph}</p>
+<p>{m.about_us_paragraph()}</p>
